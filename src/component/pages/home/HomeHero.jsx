5157ee0f32b717754b834bb7148b031200c3637e
yliu-99 <yuhanliu.0602@gmail.com>
2026-02-23 21:56:22 -0800

    texture + hero set up

--- src/component/pages/home/HomeHero.jsx
+++ src/component/pages/home/HomeHero.jsx
@@ -1,7 +1,25 @@
+// import dependancies
+
+// import components
+
 function HomeHero () {
     return (
-        <>
-        </>
+        <div className="container">
+            <div className="hero-elements">
+          <div className="circle">
+            <img src="" alt="Blue decorative circle element" />
+          </div>
+          <div className="moth">
+            <img src="" alt="Luna moth decorative illustration" />
+          </div>
+          <div className="animated-text">
+            <img src="" alt="Rotating circular text animation" />
+          </div>
+          <div className="design-text">
+            <h4>DESIGN</h4>
+          </div>
+        </div>
+        </div>
     )
 };
 
