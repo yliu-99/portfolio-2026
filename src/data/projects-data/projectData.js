b6c22e385ff95bfadc0803c2c898ef0c0926cf66
yliu-99 <yuhanliu.0602@gmail.com>
2026-02-26 11:37:59 -0800

    update projects data

--- src/data/projects-data/projectData.js
+++ src/data/projects-data/projectData.js
@@ -19,6 +19,7 @@ export const projectsData = [
         description:'A Mythbusters explainer style motion graphics video focusing on debunking the biggest personal myth -- "perfectionism',
         chips: ['motion graphics', 'storytelling', 'sound design'],
         category:'motion',
+        tools: ['after effects', 'photoshop', 'audition', 'illustrator'],
         featured: true
     },
     {
@@ -32,6 +33,7 @@ export const projectsData = [
         description:'A narrative-driven commercial exploring the relationship of two long distance friends, emphasizing the value of personal connection over external/online validation.',
         chips:['video production','storytelling','sound design'],
         category:'video',
+        tools: ['premier','audition','after effects'],
         featured: true
     },
     {
@@ -45,6 +47,7 @@ export const projectsData = [
         description:'A 2-minute talking head commercial for a fictitious mountain bike company with energetic storytelling, featuring original music made in GarageBand.',
         chips:['video prodection','storytelling','sound design','music production'],
         category:'video',
+        tools: ['premier','audition','garageband'],
         featured: true
     },
     {
@@ -58,6 +61,7 @@ export const projectsData = [
         description:'A complete branding and packaging design project for a niche fragrance line, focusing on mystical and enchanting visual storytelling.',
         chips: ['graphic design', 'brand design', 'photoshop'],
         category:'graphic design',
+        tools: ['photoshop', 'aftereffects','canva'],
         featured: false
     },
     {
@@ -67,24 +71,26 @@ export const projectsData = [
         type:'img',
         media:vancouverHorrorShowImage,
         year:'Dec 2024',
-        timeline:' Nov 14 - Dec 12',
+        timeline:' Nov 14 2024 - Dec 12 2025',
         description:'A redesign project for a Vancouver-based horror film festival focusing on improved branding.',
         chips:['graphic design', 'redesign', 'branding'],
         category:'graphic design',
+        tools: ['photoshop', 'illustrator','indesign'],
         featured: true
     },
     {
-        id:'',
-        title:'',
-        slug:'',
-        type:'',
-        media:'',
-        year:'',
-        timeline:'',
-        description:'',
-        chips:'',
-        category:'',
-        featured: true
+        id:'thrash',
+        title:'THRASH! HAIR COLOR',
+        slug:'thrash-hair-color',
+        type:'img',
+        media:thrashHairImage,
+        year:'Apr 2025',
+        timeline:'Mar 24 - Jun 3 2025',
+        description:'Brand design for a pink-inspired hair color company, featuring, logo, style guide, assets, packaging designs made in Illustrator.',
+        chips:['graphic design', 'brandin', 'illustrator'],
+        category:'graphic design',
+        tools: ['illustrator', 'photoshop', 'canva'],
+        featured: false
     },
     {
         id:'',
