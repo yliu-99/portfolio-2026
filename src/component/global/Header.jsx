be9b4bc1aa91f95ba11fba4e70139c20db9b776d
yliu-99 <yuhanliu.0602@gmail.com>
2026-03-01 19:18:28 -0800

    seo update

--- src/component/global/Header.jsx
+++ src/component/global/Header.jsx
@@ -1,5 +1,6 @@
 // import dependencies
 import { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import gsap from 'gsap';
 import LogoFull from '../../assets/global-assets/logo-main.png';
 import { useLogoRotation } from '../../hooks/useLogoRotation';
@@ -26,16 +27,16 @@ function Header() {
             <ul className='font-title text-blue uppercase text-h4 h-18 pl-12 pr-12 grid grid-cols-[1fr_auto_1fr] items-center border-b-3 border-red'>
                 <li>
                     <ul className='grid grid-cols-2'>
-                        <li className='flex justify-center'><a href="/about">About Me</a></li>
-                        <li className='flex justify-center'><a href="/projects">Projects</a></li>
+                        <li className='flex justify-center'><Link to="/about">About Me</Link></li>
+                        <li className='flex justify-center'><Link to="/projects">Projects</Link></li>
                     </ul>
                 </li>
                 <li ref={logoRef} className='home-logo flex justify-center px-10'>
-                    <a href="/"><img ref={logoImgRef} src={LogoFull} alt="Logo" className="logo-img max-w-16"/></a>
+                    <Link to="/"><img ref={logoImgRef} src={LogoFull} alt="Logo" className="logo-img max-w-16"/></Link>
                 </li>
                 <li>
                     <ul className='grid grid-cols-2'>
-                        <li className='flex justify-center'><a href="/playground">Playground</a></li>
+                        <li className='flex justify-center'><Link to="/playground">Playground</Link></li>
                         <li className='flex justify-center'><button onClick={openContact} className="contact-nav-btn">Contact</button></li>
                     </ul>
                 </li>
